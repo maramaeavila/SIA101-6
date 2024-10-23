1f94e3dc7d876ccec6c7478521a82a6133f8c628
Mara Mae <avila.maramae.orbeta@gmail.com>
2024-10-23 11:58:19 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDiOsr6bY5BDKdiBPRzDgSpurHdkkUlc3k",
+    authDomain: "sia101-d60a1.firebaseapp.com",
+    projectId: "sia101-d60a1",
+    storageBucket: "sia101-d60a1.appspot.com",
+    messagingSenderId: "258109532727",
+    appId: "1:258109532727:web:73d735dc749d2cb4ebedb2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
